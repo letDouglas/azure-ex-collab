879d1bff7169257771ad6f9e329eaf294d5e52dd
letDouglas <damir.borsetti@refactormi.com>
2024-08-07 09:18:17 +0200

    Fixed bugs

--- src/functions/apiHandlerFunction.ts
+++ src/functions/apiHandlerFunction.ts
@@ -2,31 +2,40 @@ import { HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functio
 import { fetchRandomPokemon, fetchRandomLocation, fetchRandomMove } from './../services/pokemonService';
 import { Pokemon, Location, Move } from '../utils/pokemonTypes';
 
+const fetchPokemonData = async (context: InvocationContext): Promise<Pokemon | null> => {
+  try {
+    return await fetchRandomPokemon();
+  } catch (error: any) {
+    context.log(`Errore nel recupero del Pokemon: ${error.message}`);
+    return null;
+  }
+};
+
+const fetchLocationData = async (context: InvocationContext): Promise<Location | null> => {
+  try {
+    return await fetchRandomLocation();
+  } catch (error: any) {
+    context.log(`Errore nel recupero della Location: ${error.message}`);
+    return null;
+  }
+};
+
+const fetchMoveData = async (context: InvocationContext): Promise<Move | null> => {
+  try {
+    return await fetchRandomMove();
+  } catch (error: any) {
+    context.log(`Errore nel recupero della Move: ${error.message}`);
+    return null;
+  }
+};
+
 export const apiHandlerFunction = async (request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> => {
   const startTime = Date.now();
   context.log('Elaborazione della richiesta');
 
-  let dataPokemon: Pokemon | null = null;
-  let dataLocation: Location | null = null;
-  let dataMove: Move | null = null;
-
-  try {
-    dataPokemon = await fetchRandomPokemon();
-  } catch (error: any) {
-    context.log(`Errore nel recupero del Pokemon: ${error.message}`);
-  }
-
-  try {
-    dataLocation = await fetchRandomLocation();
-  } catch (error: any) {
-    context.log(`Errore nel recupero della Location: ${error.message}`);
-  }
-
-  try {
-    dataMove = await fetchRandomMove();
-  } catch (error: any) {
-    context.log(`Errore nel recupero della Move: ${error.message}`);
-  }
+  const dataPokemon = await fetchPokemonData(context);
+  const dataLocation = await fetchLocationData(context);
+  const dataMove = await fetchMoveData(context);
 
   const endTime = Date.now();
   const executionTime = endTime - startTime;
